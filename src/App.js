91d8471d5840c6757b51602bbd113f52ddb61ca5
I4PROINFO\anmak <andrew.mak@i4pro.com.br>
2019-01-16 19:02:51 -0200

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+import Inverter, { MegaSena } from './componentes/Multi'
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Simples texto='Flexível!!!!' />
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	}
+})
